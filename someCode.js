fada1dcd4400e4454d87881818842b3b76252b4f
Alexand Gritsan <racerx8000@gmail.com>
2021-02-26 23:30:49 +0200

    done

--- someCode.js
+++ someCode.js
@@ -1,18 +1,15 @@
 function divide(a, b) {
-    if (b = 0) {
+    if (b == 0) {
         throw new Error('0 is not acceptable!')
     }
 
     return a / b;
 }
 
-<<<<<<< HEAD
 function multiply(a, b) {
     return a * b;
 }
-=======
 function calculate(a, b, c) {
     let mulitplyRes = multiply(a, b);
     return divide(mulitplyRes, c);
 }
->>>>>>> calculate func
