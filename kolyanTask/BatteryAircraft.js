10e9ce67db24cc0359eb5ffe4ea9713df79de0d2
Alexand Gritsan <racerx8000@gmail.com>
2021-03-25 15:51:38 +0200

    fixed recharge func

--- kolyanTask/BatteryAircraft.js
+++ kolyanTask/BatteryAircraft.js
@@ -1,27 +1,21 @@
 const Aircraft = require('./Aircraft');
-const AircraftTypeError = require('./AircraftTypeError');
 const BatteryChargeError = require('./BatteryChargeError');
-const AIRCRAFT_TYPES = require('./AIRCRAFT_TYPES');
 
 class BatteryAircraft extends Aircraft {
-  setType(type) {
-    if (type !== AIRCRAFT_TYPES.plane) {
-      throw new AircraftTypeError('Wrong aircraft type!');
-    }
-    super.setType(type);
-  }
-
   batteryCapacity = 5;
 
   currentBatteryCapacity = 2;
 
-  async recharge(mah) {
-    if (mah > (this.batteryCapacity - this.currentBatteryCapacity)) {
+  async recharge(ah) {
+    if (ah > (this.batteryCapacity - this.currentBatteryCapacity)) {
       throw new BatteryChargeError('Unacceptable charge capacity!');
     }
-    setTimeout(() => {
-      this.currentBatteryCapacity += mah;
-    }, mah);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.currentBatteryCapacity += ah;
+        resolve();
+      }, ah);
+    });
   }
 }
 
